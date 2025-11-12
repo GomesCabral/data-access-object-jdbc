12359ff34786fb084bbf6afe2d714e46d34042df
Gomes Cabral <phgcabral8@gmail.com>
2025-11-12 21:49:25 +0100

    create Seller class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(11, "Bob", "sellerbob@gmail.com", new Date(), 3456.0, obj);
+		System.out.println(seller);
 	}
 
 }
